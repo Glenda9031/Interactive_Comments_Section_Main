5adfaaba94a88d367bca93eb6e6d8e5f811a97ce
Glenda Joyce Reed <100007624+Glenda9031@users.noreply.github.com>
2024-11-16 13:14:32 -0600

    Update menu_avatar.js

---
+++ src/js/menu_avatar.js
@@ -0,0 +1,34 @@
+const navMenu = document.querySelector('menu');
+const usernameArray = ['maxblagun', 'amyrosen', 'ramsesmiron', 'juliusomo'];
+const defaultNavElements = `
+    <input type="checkbox" class="menu-open" name="menu-open" id="menu-open" />
+    <label class="menu-open-button" for="menu-open">
+        <span class="lines line-1"></span>
+        <span class="lines line-2"></span>
+    </label>`;
+
+function createMenuAvatarElement(name) {
+    const avatar = document.createElement('a');
+    avatar.href = "#";
+    avatar.classList.add('menu-item');
+    avatar.id = name;
+    avatar.innerHTML = `
+        <picture>
+            <source srcset="./src/images/avatars/image-${name}.webp" type="image/webp">
+            <img class="user-avatar menu-avatar" src="./src/images/avatars/image-${name}.png" alt="User Avatar">
+        </picture>
+    `;
+}
+
+avatar.addEventListener('click', () => {
+    // remove the clicked username from the array
+    const index = usernameArray.indexOf(name);
+    if (index > -1) {
+        usernameArray.splice(index, 1);
+    }
+
+    // Add the clicked username to the beginning of the array
+    usernameArray.push(name);
+
+    
+})
