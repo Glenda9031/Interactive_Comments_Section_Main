164166cf5e67ecb1aa98b634146d0bf56ba21cd4
Glenda Joyce Reed <100007624+Glenda9031@users.noreply.github.com>
2024-11-18 16:22:10 -0600

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,34 @@ let lastId = 4;
 let data = getStoredData();
 let currentUsername = data.currentUser.username;
 
+// Fetch default data from the local "data JSON" file
+const loadComments = async() => {
+    allComments.innerHTML = '';
+    let response = await fetch('../src/data.json');
+    let commentData = await response.json();
+    saveDataLocalStorage(commentData);
+    renderComments(commentData);
+};
+
+// Attach event listener to use current user
+userUnderMenu.addEventListener('click', e => {
+    if (user) {
+        currentUsername = usersUnderMenu.lastElementChild.id;
+        location.reload();
+        updateCurrentUserToLS(currentUsername);
+    }
+});
+
+// Update current user to local storage
+const updateCurrentUserToLS = (username) => {
+    data.currentUser.image.png = '../src/images/avatars/image-${updateCurrentUser}.png';
+    data.currentUser.image.webp = '../src/images/avatars/image-${updateCurrentUser}.webp';
+    data.currentUser.username = updatedCurrentUser;
+    saveDataLocalStorage(data);
+};
+
+
+
+
+
+ 
